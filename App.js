215255a2ccd8bf99bf35cd827504484ef160ca71
Luong Dao <luongdv85025@co-well.vn>
2022-10-16 12:30:00 +0700

    config app theme

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AuthProvider from './context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider as ThemeProvider } from 'react-native-paper';
 
 import AppRoutes from './navigation';
 import useFonts from './hooks/useFonts';
+import theme from './theme/theme';
 
 const queryClient = new QueryClient();
 
@@ -15,15 +17,14 @@ export default function App() {
     if (!fontsLoaded) return null;
 
     return (
-        <SafeAreaView
-            style={{ flex: 1, backgroundColor: '#ffffff' }}
-            onLayout={onLayoutRootView}
-        >
-            <QueryClientProvider client={queryClient}>
-                <AuthProvider>
-                    <AppRoutes />
-                </AuthProvider>
-            </QueryClientProvider>
-        </SafeAreaView>
+        <ThemeProvider theme={theme}>
+            <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+                <QueryClientProvider client={queryClient}>
+                    <AuthProvider>
+                        <AppRoutes />
+                    </AuthProvider>
+                </QueryClientProvider>
+            </SafeAreaView>
+        </ThemeProvider>
     );
 }
---
+++ theme/theme.js
@@ -0,0 +1,11 @@
+import { MD3DarkTheme as DefaultTheme } from 'react-native-paper';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#3C5B92',
+    },
+};
+
+export default theme;
